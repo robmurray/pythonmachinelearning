832f5010aa674a973baf49987951c6f62d8c1164
Robert Murray <rob@wildmonkeybytes.com>
2016-09-21 10:34:16 -0400

    lots of refactors to make the code easier to work with

--- skikitlearnclassifiers/ClassifierBase.py
+++ skikitlearnclassifiers/ClassifierBase.py
@@ -2,11 +2,7 @@ from skikitlearnclassifiers.ClassifierUtils import ClassifierUtils as cu
 import logging
 
 '''
-    A base class for exploring skikit-learn classifiers.
-      - loads iris dataset
-      - split data into training and test data
-      - performs feature standardization
-      - defines useful methods for predictions and plotting of data
+   
 
 '''
 
